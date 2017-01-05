a7fc3ca4ea7a8f4acc6c28c5110519218aba266b
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-01-05 12:15:40 -0200

    Implementação da função para copiar os textos do entry para o gource

--- src/model/utils.c
+++ src/model/utils.c
@@ -1,8 +1,25 @@
 #include <utils.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 char *rgba_to_hex(double red, double green, double blue){
     static char hex[8];
     sprintf(hex, "#%02X%02X%02X", (int)(red*255), (int)(green*255), (int)(blue*255));//%02X it's a lower-case hexadecimal
     return hex;
+}
+
+void copy_string(_string *destination, const char *source){
+    if(strlen(source)<destination->size){
+        strcpy(destination->value,source);
+    }else{
+        char *tmp=(char *)realloc(destination->value,strlen(source)+1);
+        if(tmp!=NULL){
+            destination->size=strlen(source)+1;
+            destination->value=tmp;
+            strcpy(destination->value,source);
+        }else{
+            destination->size=0;
+        }
+    }
 }
--- src/model/utils.h
+++ src/model/utils.h
@@ -1,6 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <gource.h>
+
 char *rgba_to_hex(double red, double green, double blue);
+
+void copy_string(_string *destination, const char *source);
 
 #endif
